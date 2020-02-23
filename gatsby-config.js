416048adf9b9e76bc77337fd33ea239adb5c8efb
Lucas Bernalte <lucas.bernalte@kairosds.com>
2020-02-23 21:43:40 +0000

    feat(cms): add netlify cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@
 module.exports = {
   /* Your site config here */
   plugins: [
+    `gatsby-plugin-netlify-cms`,
     `gatsby-plugin-sass`,
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-sharp`,
