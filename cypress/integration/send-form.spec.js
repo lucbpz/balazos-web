9cee8aa48b7626cd201349b149cf9f3f73f960a9
Lucas Bernalte Pérez <lucasbernalteperez@Ennita-MBP.local>
2020-05-15 02:46:06 +0200

    add test and add thank you page

---
+++ cypress/integration/send-form.spec.js
@@ -0,0 +1,23 @@
+describe('when a user fills out the form', async () => {
+    it('sends an email', () => {
+        cy.visit('/')
+
+        cy.createInbox().then(({ id, emailAddress }) => {
+            cy.get('input[name="name"]').type('John Doe')
+            cy.get('input[name="email"]').type('john.doe@gmail.com')
+            cy.get('input[name="subject"]').type('Test')
+            cy.get('textarea[name="message"]').type('Una nueva versión de la web de balazos se está desplegando!')
+
+            cy.get('input[type="submit"]').click()
+
+            cy.url().should('include', '/thankyou')
+            cy.get('h1').contains('Gracias por contactar con nosotros!').should('be.visible')
+
+            cy.waitForLatestEmail(id).then(latestEmail => {
+                expect(latestEmail).is.ok
+                expect(latestEmail.body).is.ok
+            })
+        });
+
+    })
+})
